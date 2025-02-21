80d5205c0c6d1b9820cbed5fb27039b99a890877
newbie <site000creator@gmail.com>
2025-02-21 15:40:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
 const { exec } = require("child_process");
-
 const app = express();
+const port = process.env.PORT || 3000;
 const FileUpload = multer({ dest: "uploads/" });
 
 app.use(express.json());
@@ -35,4 +35,4 @@ app.get("/", (req, res) => {
     res.send("Сервер работает! Используйте /upload для загрузки файлов.");
 });
 
-app.listen(3000, () => console.log("Сервер работает на порту 3000"));
+app.listen(port, () => console.log("Сервер работает на порту 3000"));
